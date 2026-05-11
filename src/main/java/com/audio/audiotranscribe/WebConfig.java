24bef227b9c8a68aaf21690113a0e854d3d61e9b
Antek <antoni.samol@gmail.com>
2026-05-11 23:24:33 +0200

    allowed mapping configured

--- src/main/java/com/audio/audiotranscribe/WebConfig.java
+++ src/main/java/com/audio/audiotranscribe/WebConfig.java
@@ -1,2 +1,18 @@
-public class WebConfig {
+package com.audio.audiotranscribe;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("{YOUR_URL}")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
